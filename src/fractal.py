2a10a38423442175b4eb36b8a87bbbda8a4121d3
SirGnip <SirGnip@users.noreply.github.com>
2023-03-06 20:26:17 -0600

    Extract magic numbers into constants

--- src/fractal.py
+++ src/fractal.py
@@ -1,29 +1,32 @@
-ESC = 10
+ESCAPE_THRESHOLD = 1.5
+STEPS_HALF = 30
+MAX_ITERATIONS = 100
+DIVISOR = 20
 
 
 def funct(z, c):
-    return z*z+c
+    return z * z + c
 
 
-def iterate(c, ct):
+def iterate(c, count):
     z = 0
     items = [z]
-    for i in range(ct):
+    for i in range(count):
         z = funct(z, c)
         items.append(z)
     return items
 
 
 def is_bound(items):
-    return max([abs(i) for i in items]) < ESC
+    return max([abs(i) for i in items]) < ESCAPE_THRESHOLD
 
 
 t = []
-for img in [i/10 for i in range(-15, 15)]:
+for img in [i / DIVISOR for i in range(-STEPS_HALF, STEPS_HALF)]:
     row = ''
-    for r in [i/10 for i in range(-15, 15)]:
+    for r in [i / DIVISOR for i in range(-STEPS_HALF, STEPS_HALF)]:
         c = complex(r, img)
-        s = iterate(c, 10)
+        s = iterate(c, MAX_ITERATIONS)
         b = is_bound(s)
         row += 'X' if b else ' '
     t.append(row)

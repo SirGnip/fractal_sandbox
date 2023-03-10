0a21b22d7163ea7749d13089d1f9c1d0a4d05289
SirGnip <SirGnip@users.noreply.github.com>
2023-03-09 18:09:26 -0600

    Optimize: Add concurrency with multiprocessing pool

--- src/fractal_arcade.py
+++ src/fractal_arcade.py
@@ -1,6 +1,7 @@
 import time
 import arcade
 import utils
+import concurrent.futures
 
 ESCAPE_THRESHOLD = 10
 MAX_ITERATIONS = 200
@@ -11,6 +12,7 @@ X_START, Y_START = -2.25, -0.9
 PIXEL_SIZE_START = 0.003  # the dimensions of each screen pixel in the Argand plane
 NUM_PIXELS_TO_MOVE = 20
 ZOOM_PERCENT = .1
+MAX_PROC_WORKERS = 8
 
 
 def iterate(c, count) -> bool:
@@ -25,17 +27,36 @@ def iterate(c, count) -> bool:
     return True
 
 
-def make_point_list(x_pos, y_pos, pixel_size, draw_step):
-    point_list = []
-    for x_pixel in range(0, X_DIMENSION, draw_step):
-        for y_pixel in range(0, Y_DIMENSION, draw_step):
-            real = x_pos + (x_pixel * pixel_size)
-            img = y_pos + (y_pixel * pixel_size)
-            c = complex(real, img)
-            is_bound = iterate(c, MAX_ITERATIONS)
-            if is_bound:
-                point_list.append((x_pixel, y_pixel))
+def do_work(x_pix_start, x_pix_end, x_off, y_off, pix_size, draw_step):
+    # print('do_work', x_pix)
+    with utils.SimpleTimer() as timer:
+        point_list = []
+        for x_pix in range(x_pix_start, x_pix_end, draw_step):
+            for y_pixel in range(0, Y_DIMENSION, draw_step):
+                real = x_off + (x_pix * pix_size)
+                img = y_off + (y_pixel * pix_size)
+                c = complex(real, img)
+                is_bound = iterate(c, MAX_ITERATIONS)
+                if is_bound:
+                    point_list.append((x_pix, y_pixel))
+    # print("worker", x_pix_start, timer.elapsed)
     return point_list
+
+
+def make_point_list(executor, x_pos, y_pos, pixel_size, draw_step):
+    chunk_size = 20
+    chunks = 0
+    futures = []
+    for x_pixel in range(0, X_DIMENSION, chunk_size):
+        futures.append(executor.submit(do_work, x_pixel, x_pixel + chunk_size, x_pos, y_pos, pixel_size, draw_step))  # non blocking
+        chunks += 1
+    print('waiting for results after scheduling chunks:', chunks)
+    results = [fut.result() for fut in futures]  # blocking
+
+    points = []
+    for r in results:
+        points.extend(r)
+    return points
 
 
 class MyFractal(arcade.Window):
@@ -45,13 +66,14 @@ class MyFractal(arcade.Window):
         self.pixel_size = PIXEL_SIZE_START
         self.draw_step = DRAW_STEP_START
 
+        self.exe = concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROC_WORKERS)
         arcade.set_background_color(arcade.color.BLACK)
         self.point_list = []
         self.recalc()
 
     def recalc(self):
         with utils.SimpleTimer() as timer:
-            self.point_list = make_point_list(self.x_pos, self.y_pos, self.pixel_size, self.draw_step)
+            self.point_list = make_point_list(self.exe, self.x_pos, self.y_pos, self.pixel_size, self.draw_step)
         print(f"{self.x_pos:.6f},{self.y_pos:.6f} pixel_size={self.pixel_size:.6f} elapsed={timer.elapsed:.2f}")
 
     def on_draw(self):
@@ -89,6 +111,7 @@ class MyFractal(arcade.Window):
             self.recalc()
         elif key in (arcade.key.Q, arcade.key.ESCAPE):
             print("Quitting")
+            self.exe.shutdown()
             self.close()
 
     def on_mouse_press(self, x, y, button, key_modifiers):
